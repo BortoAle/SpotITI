9ab299edbca492e76069bd623f24f0ca0681acc3
Alessandro Bortoluzzi <borto.ale@icloud.com>
2023-03-15 18:49:08 +0100

    Initial Commit

---
+++ SpotITI/SpotITIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotITIApp.swift
+//  SpotITI
+//
+//  Created by Alessandro Bortoluzzi on 15/03/23.
+//
+
+import SwiftUI
+
+@main
+struct SpotITIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
